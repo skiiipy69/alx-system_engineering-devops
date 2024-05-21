ca7c01a54f2c49e4a20231a0576d77573bb45330
Zouhair <darkzouhair@gmail.com>
2024-05-20 18:25:42 -0700

    Api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python3
-"""
-Extend the python script from exercise 0 to export data in JSON format.
-
-Records all completed tasks from all employees.
-Format must be: {"USER_ID": [ {"task": TASK_TITLE,
-                               "completed": TASK_COMPLETED_STATUS,
-                               "username": USERNAME"},
-                               {...}
-                            ]
-File name must be: todo_all_employees.json
-"""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
 if __name__ == "__main__":
-    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
-    tasks = requests.get("http://jsonplaceholder.typicode.com/todos").json()
-    storage = {}
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-    for user in users:
-        eid = user.get("id")
-        username = user.get("username")
-        all_tasks = []
-
-        for task in tasks:
-            if (task.get("userId") == eid and task.get("completed")):
-                temp = {}
-                temp["task"] = task.get("title")
-                temp["completed"] = task.get("completed")
-                temp["username"] = username
-                all_tasks.append(temp)
-
-        storage[eid] = all_tasks
-
-    with open("todo_all_employees.json", 'w+') as jsonfile:
-        json.dump(storage, jsonfile)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
